3cf4453efee3fe3da65bfb0a88c678d96485d909
unknown <rishabh.banshi@gmail.com>
2021-03-21 19:13:13 +0530

    Middleware authentication

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,14 @@
 const express=require('express');
 const router=express.Router();
+const auth=require('../../middleware/auth');
 
-router.get('/',(req,res)=>res.send('AUTH Route'));
+router.get('/',auth,async (req,res)=>{
+    try{
+        const user= await User.findById(req.user.id).select('-password');
+        res.json(user);
+    }catch(err){
+        res.status(500).send('Server error');
+    }
+});
 
 module.exports=router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router=express.Router();
 const {check, validationResult} = require('express-validator');
 const gravatar=require('gravatar');
 const bcrypt=require('bcryptjs');
+const config =require('config');
+const jwt =require('jsonwebtoken');
 
 const User=require('../../models/Users');
 
@@ -40,8 +42,15 @@ const salt=await bcrypt.genSalt(10);
 
 user.password=await bcrypt.hash(password,salt);
 await user.save();
- res.send('User registered');
-    // res.send('User Route');
+ const payload ={
+     user:{
+        id:user.id
+     }
+ }
+ jwt.sign(payload,config.get('jwtSecret'),{expiresIn:360000},(err,token)=>{
+    if(err) throw err;
+    res.json({token});
+ });
 }catch(err){
     console.log(err.message);
     res.status(500).send('Server error');
