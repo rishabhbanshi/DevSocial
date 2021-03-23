5c8a67725c3d126411cffe462746888bc0103284
unknown <rishabh.banshi@gmail.com>
2021-03-23 18:58:20 +0530

    Profile Schema

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,19 @@
 const express=require('express');
 const router=express.Router();
+const auth=require('../../middleware/auth');
+const User=require('../../models/Users');
+const Profile=require('../../models/Profile');
 
-router.get('/',(req,res)=>res.send('profile Route'));
+router.get('/me',auth,async(req,res)=>{
+    try{
+        const profile=await Profile.findOne({user:req.user.id}).populate('user',['name','avatar']);
+        if(!profile){
+            return res.status(400).json({mssg:"NO user Profile"});
+        }
+
+    }catch(err){
+        return res.status(500).send('Server Error');
+    }
+});
 
 module.exports=router;
