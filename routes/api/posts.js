c0307dbc0d50808969d83a949351b456ce973c1e
rishabhbanshi <rishabh.banshi@gmail.com>
2021-05-29 21:49:26 +0530

    Comment Routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,59 @@
-const express=require('express');
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
+const User = require("../../models/Users");
+const Profile = require("../../models/Profile");
+const Post = require("../../models/Post");
 
-router.get('/',(req,res)=>res.send('Post Route'));
+router.post(
+  "/",
+  [auth, [check("text", "Text is empty").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+      const post = await newPost.save();
 
-module.exports=router;
+      res.json(post);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ mssg: "No Auth" });
+    }
+    await post.remove();
+    res.json({ mssg: "Post removed" });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+module.exports = router;
