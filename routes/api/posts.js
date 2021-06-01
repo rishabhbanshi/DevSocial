fe2ad8fe307a8d5ce9116dd93e536e91eacefd9a
rishabhbanshi <rishabh.banshi@gmail.com>
2021-06-01 20:34:23 +0530

    Comment Routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -96,4 +96,58 @@ router.put("/unlike/:id", auth, async (req, res) => {
   }
 });
 
+router.post(
+  "/comment/:id",
+  [auth, [check("text", "Text is empty").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const post = await Post.findById(req.params.id);
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+      post.comments.unshift(newComment);
+      await post.save();
+
+      res.json(post.comments);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+    if (!comment) {
+      return res.status(400).json({ mssg: "No comment Found" });
+    }
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ mssg: "user not auth" });
+    }
+    const removeIndex = post.comments
+      .map((comment) => comment.user.toString())
+      .indexOf(req.user.id);
+
+    post.comments.splice(removeIndex, 1);
+    await post.save();
+    res.json(post.comments);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
